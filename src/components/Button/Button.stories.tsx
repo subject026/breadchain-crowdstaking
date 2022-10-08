44f17a4b247360ea036ce702a50fa7df412ef2f9
subject026 <subject026@protonmail.com>
2022-10-08 21:21:38 +0100

    cleanup Button story

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -2,21 +2,21 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import Button from "./Button";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "Button",
+  title: "Core/Button",
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  //   argTypes: {
-  //     backgroundColor: { control: "color" },
-  //   },
+  parameters: {
+    layout: "fullscreen",
+  },
 } as ComponentMeta<typeof Button>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof Button> = (args) => (
+  <div className="p-8">
+    <Button {...args} />
+  </div>
+);
 
 export const Regular = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Regular.args = {
   children: "Default",
 };
@@ -28,21 +28,18 @@ Disabled.args = {
 };
 
 export const Small = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Small.args = {
   children: "Small",
   variant: "small",
 };
 
 export const Large = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Large.args = {
   children: "Large",
   variant: "large",
 };
 
 export const FullWidth = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 FullWidth.args = {
   children: "Full Width",
   variant: "large",
