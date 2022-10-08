81e39097ace28cb29efe1434ad53a09963d29561
subject026 <subject026@protonmail.com>
2022-10-08 21:14:18 +0100

    refactor Header and add story

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,8 +6,6 @@ import Header from "../Header";
 import * as Main from "./ui/Main";
 import Footer from "../Footer";
 import Modal from "../Modal";
-import Logo from "../Header/Logo";
-import * as WalletDisplay from "../Header/WalletDisplay";
 
 import { getNetwork } from "../../api";
 
@@ -18,18 +16,16 @@ import {
   setNetwork,
   setNetworkConnected,
 } from "../../features/network/networkSlice";
-import { formatAddress } from "@/util";
 import { setIsLoaded } from "../../features/font/fontSlice";
 
 import Toast from "../Toast/Toast";
 import { Pantry } from "../Pantry";
 import Index from "../../routes/Index";
-import DesktopNavigation from "../Navigation/DesktopNavigation";
 import About from "../../routes/Info";
-import MobileNavigationToggle from "../Header/MobileNavigationToggle";
 import SiteTitle from "../SiteTitle/SiteTitle";
 import { EToastType, setToast } from "../../features/toast/toastSlice";
 import { useAccount, useNetwork } from "wagmi";
+import TextLink from "../TextLink/TextLink";
 
 const App: React.FC<React.PropsWithChildren<unknown>> = () => {
   const dispatch = useAppDispatch();
@@ -82,19 +78,8 @@ const App: React.FC<React.PropsWithChildren<unknown>> = () => {
       {toast.type !== null && toast.message !== null && (
         <Toast type={toast.type} message={toast.message} />
       )}
-      <Header>
-        <Logo />
-        <DesktopNavigation />
-        <WalletDisplay.Container>
-          {<WalletDisplay.Network />}
-          {accountAddress && (
-            <WalletDisplay.Address>
-              {formatAddress(accountAddress)}
-            </WalletDisplay.Address>
-          )}
-        </WalletDisplay.Container>
-        <MobileNavigationToggle />
-      </Header>
+      <Header />
+
       <SiteTitle />
       <Main.Main>
         <Routes>
@@ -105,9 +90,9 @@ const App: React.FC<React.PropsWithChildren<unknown>> = () => {
       </Main.Main>
 
       <Footer>
-        <Link to="/pantry" className="opacity-0 hover:opacity-100 px-4 py-2">
+        <TextLink variant="hidden" to="/pantry">
           pantry
-        </Link>
+        </TextLink>
       </Footer>
     </AppContainer>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,38 @@
-import React from "react";
+import { ReactNode } from "react";
+import { useAccount } from "wagmi";
+import { formatAddress } from "@/util";
+import { DesktopNavigation } from "../Navigation";
+import Logo from "./Logo";
+import MobileNavigationToggle from "./MobileNavigationToggle";
+import * as WalletDisplay from "./WalletDisplay";
 
-const Header: React.FC<React.PropsWithChildren<unknown>> = ({ children }) => {
+const Container = ({ children }: { children: ReactNode }) => {
   return (
     <header className="bg-breadgray-100">
       <div className="max-w-6xl m-0 mx-auto p-6 md:px-8 flex h-24">
         {children}
       </div>
     </header>
+  );
+};
+
+const Header = () => {
+  const { address: accountAddress } = useAccount();
+
+  return (
+    <Container>
+      <Logo />
+      <DesktopNavigation />
+      <WalletDisplay.Container>
+        {<WalletDisplay.Network />}
+        {accountAddress && (
+          <WalletDisplay.Address>
+            {formatAddress(accountAddress)}
+          </WalletDisplay.Address>
+        )}
+      </WalletDisplay.Container>
+      <MobileNavigationToggle />
+    </Container>
   );
 };
 
